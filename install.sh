7e7bd481bfec3a07fb24de7d75ec58547088afca
Keyshawn Butts <kjbappsllc@gmail.com>
2019-04-07 23:09:08 -0400

    added homebrew configurations

--- install.sh
+++ install.sh
@@ -17,5 +17,50 @@ BOTNAME="Jeeves"
 # Intro
 # ###########################################################
 bot "Hi ${USER}, im ${BOTNAME}"
-sleep 0.5
+sleep 2
 bot "I am going to be configuring your system for you."
+bot "Please stay here so you can enter some passwords and answer some questions for me!"
+bot "Don't worry, I am clean and won't do anything with them."
+sleep 2
+
+# ###########################################################
+# Pre Homebrew installations
+# ###########################################################
+bot "Configuring command line build tools"
+running "checking xcode build tools ..."
+xcode-select --install > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+    info "They are not installed, please follow through with the steps."
+    info "Also, please enter your password."
+    sudo xcode-select -s /Applications/Xcode.app/Contents/Developer > /dev/null 2>&1
+    sudo xcodebuild -license accept > /dev/null 2>&1
+    ok "command line build tools are installed"
+else
+    info "command line build tools are already installed"
+fi
+sleep 0.5
+
+# ###########################################################
+# Homebrew Installation and configuration
+# ###########################################################
+bot "Configuring Homebrew"
+running "checking homebrew..."
+brew_bin=$(command -v brew) 2>&1 > /dev/null
+
+if [[ $? != 0 ]]; then
+    info "homebrew is not installed"
+    action "installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [[ $? != 0 ]]; then
+        error "unable to install homebrew, aborting!"
+    fi
+    ok "homebrew is installed"
+else
+    info "homebrew is already installed"
+    action "updating homebrew"
+    brew update
+    ok "brew is updated"
+    action "upgrading current packages"
+    brew upgrade
+    ok "brew packages are upgraded"
+fi
--- utils/display.sh
+++ utils/display.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/env bash
 
 # Colors
-BLUE="\033[1;34m"
+BLUEB="\033[1;34m"
+BLUE="\033[0;34m"
+GREENB="\033[1;32m"
+YELLOW="\033[0;33m"
+YELLOWB="\033[1;33m"
+REDB="\033[1;31"
+PURPLEB="\033[1;35m"
 BOLD="\033[1m"
 NONE="\033[0m"
 
 # Echo Utils
 bot () {
-    echo -e "${BLUE}{•̃_•̃}${NONE}: ${BOLD}${1}${NONE}"
+    echo -e "\n${BLUEB}{•̃_•̃}${NONE}: ${BOLD}${1}${NONE}"
+}
+
+running() {
+    echo -e "${YELLOW} ⇒ ${NONE}${1}"
+}
+
+ok() {
+    echo -e "${GREENB}[ok]${NONE} ${1}"
+}
+
+info () {
+    echo -e "${PURPLEB}[info]${NONE} ${1}"
+}
+
+action () {
+    echo -e "${YELLOWB}[action]${NONE} ${1}"
+}
+
+error () {
+    echo -e "${REDB}[error]${NONE} ${1}"
+    exit 2
 }
