7212a63f3969f3b843e2f421bba34406acb6a8f8
Keyshawn J. Butts <keyshawn_j_butts@homedepot.com>
2019-04-08 15:39:51 -0400

    added defaults to the beginning of the script

--- macos/install.sh
+++ macos/install.sh
@@ -11,17 +11,69 @@ unset file;
 # ###########################################################
 OS=$(uname -s)
 USER=$(whoami)
-BOTNAME="Jeeves"
+BOTNAME="Jarvis"
 
 # ###########################################################
 # Intro
 # ###########################################################
-bot "Hi ${USER}, im ${BOTNAME}"
+bot "Hi ${USER}, i'm ${BOTNAME}"
 sleep 2
 bot "I am going to be configuring your system for you."
 bot "Please stay here so you can enter some passwords and answer some questions for me!"
 bot "Don't worry, I am clean and won't do anything with them."
+sleep 1
+bot "Starting configuration tasks ..."
 sleep 2
+
+# ###########################################################
+# System defaults
+# ###########################################################
+bot "Setting up system defaults"
+# Close System Preferences just in case
+osascript -e 'tell application "System Preferences" to quit'
+ok "closed system preferences"
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+ok "set default to save to disk not iCloud"
+
+# Save screenshots as PNG
+defaults write com.apple.screencapture type -string "png"
+ok "set screenshots to save as png format"
+
+# Set plain text as default format in TextEdit
+defaults write com.apple.TextEdit RichText -int 0
+ok "set plain text as default in RichText"
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# Bottom left >> Put display to sleep
+defaults write com.apple.dock wvous-bl-corner -int 10
+defaults write com.apple.dock wvous-bl-modifier -int 0
+ok "set bottom left hot corner to put display to sleep"
+
+# Set dark theme
+info "need password to change theme to dark ..."
+sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
+ok "set theme to dark theme"
+
+### Safari ###
+
+# Privacy: don’t send search queries to Apple
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+ok "set privacy to not send search queries to apple"
+sleep 0.5
 
 # ###########################################################
 # Pre Homebrew installations
@@ -58,9 +110,9 @@ if [[ $? != 0 ]]; then
 else
     info "homebrew is already installed"
     action "updating homebrew"
-    brew update
+    brew update > /dev/null 2>&1
     ok "brew is updated"
     action "upgrading current packages"
-    brew upgrade
+    brew upgrade > /dev/null 2>&1
     ok "brew packages are upgraded"
 fi
